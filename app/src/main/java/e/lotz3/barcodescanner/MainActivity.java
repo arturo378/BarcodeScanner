b5ebf3938f866d9f59f535a722b712dc1836a0ae
lotz378 <amadrid378@hotmail.com>
2018-03-09 07:09:12 -0600

    Startup

--- app/src/main/java/e/lotz3/barcodescanner/MainActivity.java
+++ app/src/main/java/e/lotz3/barcodescanner/MainActivity.java
@@ -2,6 +2,11 @@ package e.lotz3.barcodescanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_scan);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                final DatabaseReference myRef = database.getReference("Description");
+                myRef.setValue("phone");
+                setContentView(R.layout.activity_main);
+            }
+        });
     }
 }
