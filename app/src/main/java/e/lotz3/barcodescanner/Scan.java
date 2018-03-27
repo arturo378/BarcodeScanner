0a78efa1498347727276edcc52206e47b0f1f522
lotz378 <amadrid378@hotmail.com>
2018-03-26 21:52:15 -0500

    reading works!!

--- app/src/main/java/e/lotz3/barcodescanner/Scan.java
+++ app/src/main/java/e/lotz3/barcodescanner/Scan.java
@@ -4,35 +4,117 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class Scan extends AppCompatActivity implements View.OnClickListener  {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    int minteger = 0;
+public class Scan extends AppCompatActivity   {
 
+    FirebaseDatabase mFirebaseDatabase;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    DatabaseReference myRef;
+    Button retrieve;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
-        findViewById(R.id.button_decrease).setOnClickListener(this);
-        findViewById(R.id.button_increase).setOnClickListener(this);
-    }
+        retrieve = (Button) findViewById(R.id.scan_button_add);
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        final EditText supplier = (EditText)findViewById(R.id.scan_text_supplier);
+        final EditText brand = (EditText)findViewById(R.id.scan_text_brand);
+        final EditText category = (EditText)findViewById(R.id.scan_text_category);
+        final EditText description = (EditText)findViewById(R.id.scan_text_description);
+        final EditText model = (EditText)findViewById(R.id.scan_text_model);
 
 
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.button_decrease:
+    retrieve.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FirebaseUser user = mAuth.getCurrentUser();
+            String userID = user.getUid();
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference(userID).child("Items").child("-L7aBzAt7TKek-BfbK9_").child("Brand");
+            DatabaseReference myRef2 = database.getReference(userID).child("Items").child("-L7aBzAt7TKek-BfbK9_").child("Category");
+            DatabaseReference myRef3 = database.getReference(userID).child("Items").child("-L7aBzAt7TKek-BfbK9_").child("Description");
+            DatabaseReference myRef4 = database.getReference(userID).child("Items").child("-L7aBzAt7TKek-BfbK9_").child("Model");
+            DatabaseReference myRef5 = database.getReference(userID).child("Items").child("-L7aBzAt7TKek-BfbK9_").child("Supplier");
 
 
 
-                break;
-            case R.id.button_increase:
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    brand.setText(dataSnapshot.getValue().toString());
+                }
 
-                break;
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
+                }
+            });
+            myRef2.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    category.setText(dataSnapshot.getValue().toString());
+                }
 
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            myRef3.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    description.setText(dataSnapshot.getValue().toString());
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            myRef4.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    model.setText(dataSnapshot.getValue().toString());
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            myRef5.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    supplier.setText(dataSnapshot.getValue().toString());
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
+    });
+
+
     }
+
+
+
 }
